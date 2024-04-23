99e65127cf454568ce8f770af2d57b3d9be13d44
yasi <mohamedyasin7558@gmail.com>
2024-04-23 17:59:53 +0530

    setup static url

--- onlinestore/main/urls.py
+++ onlinestore/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login/', views.login, name='login'),
@@ -8,3 +10,5 @@ urlpatterns = [
 ]
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
