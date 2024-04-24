9e259a2c3219e8c8e449d4a60f77ede3606a54df
yasi <mohamedyasin7558@gmail.com>
2024-04-24 15:29:33 +0530

    finished registration

--- onlinestore/main/urls.py
+++ onlinestore/main/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views  as auth_views
+from .forms import LoginForm
+
+
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'index.html', authentication_form = LoginForm), name = 'login'),
     path('register/', views.registration, name='register'),
+    
     
 ]
 
--- onlinestore/main/views.py
+++ onlinestore/main/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignupForm
+
 
 # Create your views here.
 
 
-def login(request):
+# def login(request):
 
-    return render(request, 'index.html')
+#     return render(request, 'index.html')
 
 def registration(request):
-    return render(request, 'registration.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    form = SignupForm()
+
+    return render(request, 'registration.html',{
+        'form' : form
+    })
+
